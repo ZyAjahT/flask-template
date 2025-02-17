e53bc2e705fc3066cce8b08bda829dbc157cd795
ZyAjahT <zyajah@gmail.com>
2025-02-16 19:49:47 -0500

    Added some error handling

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ def home():
     data = req.json()
     return render_template("index.html", data = data)
 
-@app.route("/pastComic")
-def pastComic():
-    URL = "https://xkcd.com/info.0.json"
-    req = requests.get(url = URL)
-    data = req.json()
-    return render_template("index.html", data = data)   
+@app.route("/pastComic/<comicNum>")
+def pastComic(comicNum):
+    if(comicNum.isdigit() and int(comicNum) <= 3000):
+        URL = "https://xkcd.com/" + comicNum + "/info.0.json"
+        req = requests.get(url = URL)
+        data = req.json()
+        return render_template("pastComic.html", data = data)   
+    else:
+        return render_template("errorPage.html")
 
+    
 @app.route("/album")
 def album():
     return render_template("album.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
